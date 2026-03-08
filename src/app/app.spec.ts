dda2057b6ba1b9ac848bc0022233e3d94a3d9efa
Za-Qar <za.qa@outlook.com>
2026-03-08 23:29:48 +0000

    Update readme - resolve test failures

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
+
 import { App } from './app';
+import { appConfig } from './app.config';
 import { TaskTestData } from './test/test.data';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: appConfig.providers,
     }).compileComponents();
   });
 
